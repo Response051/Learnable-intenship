484613d7acad7f422452f5af81fc07da6a646162
Ibeawuchi Sopuru George <platoon1277@gmail.com>
2022-12-13 13:58:04 +0100

    HTML stucture for An Animal trading card

---
+++ Learnable/learnable-projects/P2/index.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")();
+console.log("Hello world!");
+function check(a, b) {
+  return a + b;
+}
+
+console.log(check(2, 9));
+
+let ask = prompt(" who are u");
+
+console.log(ask);
